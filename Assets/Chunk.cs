50c1d317179b4e17092b5bd186d1ad06d7c381a0
James <jamescarterbell@gmail.com>
2019-07-19 00:58:08 -0400

    Initial commit

---
+++ Assets/Chunk.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MarchingCubes;
+using Unity.Mathematics;
+
+[RequireComponent (typeof(MeshRenderer), typeof(MeshFilter))]
+public class Chunk : MonoBehaviour
+{
+    public int dimension;
+    public float cubeSize;
+
+    private int[] field;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        field = new int[dimension * dimension * dimension];
+
+        float3 worldPos = transform.position;
+
+        Func<float3, bool> newFunc = (float3 pos) =>
+        {
+            return (Perlin.Noise((pos + worldPos)/25) > 0);
+        };
+
+
+        StartCoroutine(JobMarching.GetMesh(transform.position, dimension, cubeSize, newFunc, .001f, 20, SetMesh));
+    }
+
+    private void SetMesh(Tuple<Vector3[], int[]> newMeshInfo)
+    {
+        Mesh m = new Mesh();
+        m.vertices = newMeshInfo.Item1;
+        m.triangles = newMeshInfo.Item2;
+        m.RecalculateNormals();
+
+        var mf = GetComponent<MeshFilter>();
+        mf.mesh = m;
+    }
+
+    private int CoordToIndex(int x, int y, int z)
+    {
+        return x + y * dimension + z * dimension * dimension;
+    }
+
+}
